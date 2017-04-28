b1360761b35f773088cd0a37f27a840e00503747
zahodinikola <zahodinikola@gmail.com>
2017-04-28 05:42:36 +0300

    changed arguments list given to controller

--- src/js/controllers/pushButton.js
+++ src/js/controllers/pushButton.js
@@ -1,21 +1,8 @@
-riseTemperature.controller("pushButton", ['$scope',
+riseTemperature.controller("pushButton", 
 function($scope) {
     $scope.whatTemperature = 10;
     
     $scope.push = function() {
         $scope.whatTemperature += 10;
     }
-}])
-
-
-// app.controller("ctrlStudents", ['$scope',
-//     function($scope) {
-//         $scope.students_counter = 144;
-
-//         $scope.sessiyaPrishla = function() {
-//             if ($scope.students_counter > 20) {
-//                 $scope.students_counter -= 10;
-//             }
-//         }
-//     }
-// ])
+})
